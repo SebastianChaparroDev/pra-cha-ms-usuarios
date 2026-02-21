726dae3537e0f33411a0d6d29f81f2e2009d1499
Sebastian Chaparro <50222699+JSebastianChaparro@users.noreply.github.com>
2026-02-20 20:22:41 -0500

    cfeat: creación de estructura y configuración base

---
+++ src/main/java/com/pragma/challenge/pra_cha_ms_plazoleta/PraChaMsPlazoletaApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.challenge.pra_cha_ms_plazoleta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraChaMsPlazoletaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraChaMsPlazoletaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pragma/challenge/pra_cha_ms_plazoleta/PraChaMsPlazoletaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.challenge.pra_cha_ms_plazoleta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraChaMsPlazoletaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
